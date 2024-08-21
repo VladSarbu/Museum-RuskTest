825da8c8655c36411b6f5ee2fc98f8434a90922a
VladSarbu <sarbu.vlad91@gmail.com>
2024-08-21 23:37:09 +0300

    test

---
+++ src/main/java/ro/rusk/test/Museum_RuskTest/MuseumRuskTestApplication.java
@@ -0,0 +1,19 @@
+package ro.rusk.test.Museum_RuskTest;
+
+import org.junit.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@SpringBootApplication
+public class MuseumRuskTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuseumRuskTestApplication.class, args);
+	}
+
+
+
+
+
+}
